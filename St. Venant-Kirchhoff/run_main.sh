1b3e2e782a9a6ad2c71d4b7d9caea17593e6db88
rathod-gaurav <rathod.gauravvinod2@gmail.com>
2026-03-21 15:06:17 -0500

    generated gprof and perf profiler reports

--- St. Venant-Kirchhoff/run_main.sh
+++ St. Venant-Kirchhoff/run_main.sh
@@ -1,3 +1,16 @@
-g++ -I /home/grathod/Downloads/eigen-5.0.0/eigen-5.0.0 main.cpp -Wall -O3 -std=c++17 -lcurl -o main
-# /mnt/c/Users/ratho/Downloads/eigen-5.0.0/eigen-5.0.0 main.cpp 
-./main
+# g++ -I /home/grathod/Downloads/eigen-5.0.0/eigen-5.0.0 main.cpp -Wall -O3 -std=c++17 -lcurl -o main
+# # /mnt/c/Users/ratho/Downloads/eigen-5.0.0/eigen-5.0.0 main.cpp 
+# ./main
+
+
+# gprof
+g++ -I /home/grathod/Downloads/eigen-5.0.0/eigen-5.0.0 main.cpp -pg -O2 -lcurl -o main_gprof
+./main_gprof
+gprof main_gprof gmon.out > analysis_gprof.txt
+
+# perf
+g++ -I /home/grathod/Downloads/eigen-5.0.0/eigen-5.0.0 main.cpp -g -O2 -lcurl -o main_perf
+perf record -g ./main_perf
+perf report #visual report in the terminal 
+perf stat ./main_perf #a top-down report of the program's performance, including metrics such as CPU cycles, instructions, cache misses, etc.
+perf report --stdio > perf_report.txt #save the report to a text file
